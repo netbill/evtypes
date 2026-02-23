05bd6821a96ce11355cb743a2f87f39a642c65c1
paranoideed <yehorushakov@gmail.com>
2026-02-23 17:08:42 +0200

    first commit

---
+++ profiles.go
@@ -0,0 +1,33 @@
+package evtypes
+
+import "time"
+
+const ProfilesTopicV1 = "profiles.v1"
+
+const ProfileUpdatedEvent = "profile.updated"
+
+type ProfileUpdatedPayload struct {
+	AccountID   uuid.UUID `json:"account_id"`
+	Username    string    `json:"username"`
+	Official    bool      `json:"official"`
+	Pseudonym   *string   `json:"pseudonym,omitempty"`
+	Description *string   `json:"description,omitempty"`
+	AvatarKey   *string   `json:"avatar_key,omitempty"`
+
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+const ProfileCreatedEvent = "profile.created"
+
+type ProfileCreatedPayload struct {
+	AccountID uuid.UUID `json:"account_id"`
+	Username  string    `json:"username"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+const ProfileDeletedEvent = "profile.deleted"
+
+type ProfileDeletedPayload struct {
+	AccountID uuid.UUID `json:"account_id"`
+	DeletedAt time.Time `json:"deleted_at"`
+}
